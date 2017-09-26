830d729e016a68c6ae186795d2f211988a19c2fa
Karoly Negyesi <chx1975@gmail.com>
2017-09-27 00:03:54 +0200

    small restyle

--- src/Wbm3CmDeriver.php
+++ src/Wbm3CmDeriver.php
@@ -13,12 +13,14 @@ class Wbm3CmDeriver extends DeriverBase {
     foreach (\Drupal::state()->get('wbm3cm') as $entity_type_id => $bundles) {
       /** @var \Drupal\Core\Entity\ContentEntityTypeInterface $entity_type */
       $entity_type = \Drupal::entityTypeManager()->getDefinition($entity_type_id);
-      $definition = $base_plugin_definition;
-      $definition['source']['revision_data_table'] = $entity_type->getRevisionDataTable();
       $revision_id_field = $entity_type->getKey('revision_id');
-      $definition['source']['revision_id_field'] = $revision_id_field;
-      $definition['source']['bundle_field'] = $entity_type->getKey('bundle');
-      $definition['source']['bundles'] = $bundles;
+      $definition = $base_plugin_definition;
+      $definition['source'] += [
+        'revision_data_table' => $entity_type->getRevisionDataTable(),
+        'revision_id_field' => $revision_id_field,
+        'bundle_field' => $entity_type->getKey('bundle'),
+        'bundles' => $bundles,
+      ];
       $definition['process'][$revision_id_field] = 'revision_id';
       if (!isset($definition['destination']['plugin'])) {
         $definition['destination']['plugin'] = "nentity_revision:$entity_type_id";
