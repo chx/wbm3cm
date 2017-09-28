c3813c7db7cef8de3258c0668f5b98fe70f9ab02
Karoly Negyesi <chx1975@gmail.com>
2017-09-28 20:17:23 +0200

    doh node.type is not revisionable

--- src/Plugin/migrate/source/Wbm3Cm.php
+++ src/Plugin/migrate/source/Wbm3Cm.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\wbm3cm\Plugin\migrate\source;
 
+use Drupal\Core\State\StateInterface;
 use Drupal\migrate\Plugin\migrate\source\SqlBase;
+use Drupal\migrate\Plugin\MigrationInterface;
 
 /**
  *
@@ -13,16 +15,24 @@ use Drupal\migrate\Plugin\migrate\source\SqlBase;
  */
 class Wbm3Cm extends SqlBase {
 
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, StateInterface $state) {
+    $configuration['key'] = 'default';
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration, $state);
+  }
+
+
   /**
    * @return \Drupal\Core\Database\Query\SelectInterface
    */
   public function query() {
     $query = $this->select($this->configuration['revision_data_table'], 'r');
-    $query->addField('r', $this->configuration['revision_id_field'], 'revision_id');
+    $revision_id_field = $this->configuration['revision_id_field'];
+    $query->addField('r', $revision_id_field, 'revision_id');
     $query->addField('r', 'langcode');
     if (!empty($this->configuration['moderation_state'])) {
       $query->addField('r', 'moderation_state');
     }
+    $query->innerJoin($this->configuration['data_table'], 'd', "USING($revision_id_field)");
     $query->condition($this->configuration['bundle_field'], $this->configuration['bundles'], 'IN');
     return $query;
   }
