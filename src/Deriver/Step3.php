62ed83f569094b5bab0c701b46a5e3ba7c7d2c10
Karoly Negyesi <chx1975@gmail.com>
2017-09-28 23:06:35 +0200

    step3 fixes

--- src/Deriver/Step3.php
+++ src/Deriver/Step3.php
@@ -5,7 +5,11 @@ namespace Drupal\wbm3cm\Deriver;
 class Step3 extends Step2 {
 
   protected function addMore(array $definition, $entity_type_id) {
-    $definition['moderation_state']['migration'] = "wbm3cm_step1:$entity_type_id";
+    $definition['process']['moderation_state']['migration'] = "wbm3cm_step1:$entity_type_id";
+    $definition['process']['moderation_state']['source'] = [
+      $definition['source']['revision_id_field'],
+      $definition['source']['langcode_field'],
+    ];
     return parent::addMore($definition, $entity_type_id);
   }
 
