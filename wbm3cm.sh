6a6ef4947e93237ddaeaa4e04592c23f7f7e0984
Karoly Negyesi <chx1975@gmail.com>
2017-09-29 00:50:10 +0200

    and a little glue, for nodes, for now

---
+++ wbm3cm.sh
@@ -0,0 +1,7 @@
+drush mi wbm3cm_step1:node
+drush mi wbm3cm_step2:node
+drush ev 'Drupal\wbm3cm\Wbm3CmConfig::save()'
+drush pmu -y workbench_moderation
+drush en -y content_moderation
+drush ev 'Drupal\wbm3cm\Wbm3CmConfig::restore()'
+drush mi wbm3cm_step3:node
