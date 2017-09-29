c7253dea93c842f4d998d138df477318da9e54e0
Karoly Negyesi <chx1975@gmail.com>
2017-09-29 16:02:11 +0200

    another core bug related to entity revision and translation

--- src/Plugin/migrate/destination/EntityRevision.php
+++ src/Plugin/migrate/destination/EntityRevision.php
@@ -10,8 +10,38 @@ use Drupal\migrate\Plugin\migrate\destination\EntityRevision as BrokenEntityRevi
  * Provides entity revision destination plugin w/ working translations.
  *
  * No annotation, this is used instead of the broken core class.
+ *
+ * Two bugs:
+ *
+ * 1. getEntity loses the entity updateEntity returns.
+ * 2. getIds does not take the translations configuration into consideration.
  */
 class EntityRevision extends BrokenEntityRevision {
+
+  protected function getEntity(Row $row, array $old_destination_id_values) {
+    $revision_id = $old_destination_id_values ?
+      reset($old_destination_id_values) :
+      $row->getDestinationProperty($this->getKey('revision'));
+    if (!empty($revision_id) && ($entity = $this->storage->loadRevision($revision_id))) {
+      $entity->setNewRevision(FALSE);
+    }
+    else {
+      $entity_id = $row->getDestinationProperty($this->getKey('id'));
+      $entity = $this->storage->load($entity_id);
+
+      // If we fail to load the original entity something is wrong and we need
+      // to return immediately.
+      if (!$entity) {
+        return FALSE;
+      }
+
+      $entity->enforceIsNew(FALSE);
+      $entity->setNewRevision(TRUE);
+    }
+    $entity = $this->updateEntity($entity, $row);
+    $entity->isDefaultRevision(FALSE);
+    return $entity;
+  }
 
   public function getIds() {
     $ids = [];
